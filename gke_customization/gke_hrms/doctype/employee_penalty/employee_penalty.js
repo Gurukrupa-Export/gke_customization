75ec6aa7cbc536645df039e854256eb8ea81c8a5
Gurukrupa-Export <129185339+Gurukrupa-Export@users.noreply.github.com>
2024-03-29 10:25:35 +0530

    employee _ penalty and employee punch error report

--- gke_customization/gke_hrms/doctype/employee_penalty/employee_penalty.js
+++ gke_customization/gke_hrms/doctype/employee_penalty/employee_penalty.js
@@ -2,29 +2,8 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Employee Penalty", {
-    refresh(frm) {        
-        calculate_total(frm);
+    refresh() {        
+
     },
 });
 
-frappe.ui.form.on("Employee Penalty Detail", {
-    penalty_amount(frm, cdt, cdn) {
-        calculate_total(frm);
-    },
-});
-
-function calculate_total(frm) {
-    let total_penalty_amount = 0;
-
-    if (frm.doc.penalty_type) {
-        frm.doc.penalty_type.forEach(function (d) {
-            total_penalty_amount += d.penalty_amount;
-        });
-    }
-
-    let ctc = frm.doc.cost_to_company_ctc;
-    let advanced_cash = ctc - total_penalty_amount;
-
-    frm.set_value('total_penalty_amount', total_penalty_amount);
-    frm.set_value('advanced_cash', advanced_cash);
-}
