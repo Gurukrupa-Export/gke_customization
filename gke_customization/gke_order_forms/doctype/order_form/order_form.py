1adbfca69e325abc761afe747867e2caf62665a1
Gurukrupa-Export <129185339+Gurukrupa-Export@users.noreply.github.com>
2025-01-06 10:35:21 +0530

    Update order_form.py

--- gke_customization/gke_order_forms/doctype/order_form/order_form.py
+++ gke_customization/gke_order_forms/doctype/order_form/order_form.py
@@ -143,6 +143,7 @@ def make_cad_order(source_name, target_doc=None, parent_doc = None):
 				item_type = "Only Variant"
 	elif design_type == 'Sketch Design':
 		attribute_list = make_atribute_list(source_name)
+		variant_of = frappe.db.get_value("Item",design_id,"variant_of")
 		validate_variant_attributes(variant_of,attribute_list)
 		item_type = "No Variant No Suffix"
 		bom_or_cad = 'CAD'
