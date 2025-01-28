7706078f222137192b9db3a6f8b834b79f5fb70f
prachi-gurukrupa <prachi_b@gkexport.com>
2025-01-28 10:47:51 +0530

    Added a doctypes as a customer master update and supplier master update this doctypes are replica of supplier and customer

---
+++ gke_customization/gke_price_list/doctype/supplier_update_master/supplier_update_master.js
@@ -0,0 +1,18 @@
+// // Copyright (c) 2025, Gurukrupa Export and contributors
+// // For license information, please see license.txt
+
+// // frappe.ui.form.on("Supplier Update Master", {
+// // 	refresh(frm) {
+
+// // 	},
+// // });
+// // Copyright (c) 2025, Gurukrupa Export and contributors
+// // For license information, please see license.txt
+
+// // frappe.ui.form.on("Supplier Update Master", {
+// // 	refresh(frm) {
+
+// // 	},
+// // });
+
+
