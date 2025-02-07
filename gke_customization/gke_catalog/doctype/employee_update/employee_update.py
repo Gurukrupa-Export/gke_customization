9055a8f846f2e636ac02fef0e6c1c6691613f74e
Dharm Vaghasiya <dharm_v@gkexport.com>
2025-02-07 12:06:22 +0530

    Update Employee Update json and Py file

--- gke_customization/gke_catalog/doctype/employee_update/employee_update.py
+++ gke_customization/gke_catalog/doctype/employee_update/employee_update.py
@@ -13,22 +13,29 @@ class EmployeeUpdate(Document):
 
 	def on_submit(self):
 		filed_list = [
-			"first_name", "middle_name", "last_name", "image", "date_of_birth", "city", "state", 
-			"health_insurance_status","sum_assured_","accident_insurance_provider","accident_insurance_status",
-			"sum_assured_accident_insurance_","accident_insurance_no","is_esic_applicable","_esic_number",
+			"first_name", "middle_name", "last_name", "image", "date_of_birth", 
 			"salutation","gender","custom_notice_dayes", "employment_type","custom_probation_period_days",
 			"date_of_joining","contract_end_date", "old_employee_code", "old_punch_id","status",
-			"company", "department", "designation","operation","branch",
+			"company", "department", "designation","operation","branch","apprentice_contract_no",
+			"apprentice_register_no","scheduled_confirmation_date","final_confirmation_date","date_of_retirement",
+			"prefered_contact_email","prefered_email","unsubscribed","expense_approver","leave_approver","salary_currency",
+			"branch_name","reports_to","reporting_employee_name_",
 			"cell_number", "personal_email", "company_email", "current_address", "current_accommodation_type",
-			"same_as_current_address", "permanent_address","permanent_accommodation_type", 
+			"same_as_current_address", "permanent_address","permanent_accommodation_type","city", "state", 
+			"health_insurance_status","sum_assured_","accident_insurance_provider","accident_insurance_status",
+			"sum_assured_accident_insurance_","accident_insurance_no","is_esic_applicable","_esic_number",
 			"attendance_device_id","allowed_personal_hours","product_incentive_applicable","default_shift",
 			"holiday_list","shift_request_approver","salary_mode","ctc",
 			"employee_name_as_per_bank_account", "bank_name", "bank_ac_no", "ifsc_code", "micr_code", "iban", "marital_status",
-			"family_background", "blood_group", "health_details", "health_insurance_provider", "health_insurance_no", "passport_number", "valid_upto", "date_of_issue", "place_of_issue",
-			"religon", "employee_cast", "reference_type", "reference_employee_code", "reference_employee_name", "reference_contact",
-			"reference_address", "aadhar_number", "name_as_per_aadhar", "driving_licence_no", "name_as_per_driving_license", "driving_license_valid_upto_date", "relative_employee_code", "relative_employee_name",
-			"employee_relative_relation", "resignation_letter_date", "relieving_date", "held_on", "new_workplace", "leave_encashed", "encashment_date", "reason_for_leaving", "feedback", "is_pf_applicable",
-			"pf_joining_date","pan_number", "name_as_pe_pan", "provident_fund_account", "handicap_certificate_date", "handicap_percenatge", "is_physical_handicap", "uan_number"
+			"family_background", "blood_group", "health_details", "health_insurance_provider", "health_insurance_no", "passport_number", 
+			"valid_upto", "date_of_issue", "place_of_issue","religon", "employee_cast", "reference_type", 
+			"reference_employee_code", "reference_employee_name", "reference_contact",
+			"reference_address", "aadhar_number", "name_as_per_aadhar", "driving_licence_no", "name_as_per_driving_license", 
+			"driving_license_valid_upto_date","relative_employee_code", "relative_employee_name",
+			"employee_relative_relation", "resignation_letter_date", "relieving_date", "held_on", "new_workplace", 
+			"leave_encashed", "encashment_date", "reason_for_leaving", "feedback", "is_pf_applicable",
+			"pf_joining_date","pan_number", "name_as_pe_pan", "provident_fund_account", "handicap_certificate_date", 
+			"handicap_percenatge", "is_physical_handicap", "uan_number","variable_end_date","variable_start_date","variable_in"
 		]
 		
 		e_json = {
@@ -51,8 +58,7 @@ class EmployeeUpdate(Document):
 		e_diff_filed = [
 			"full_name", "operation", "city", "state", "health_insurance_status", "sum_assured_",
 			"accident_insurance_provider", "accident_insurance_status", "sum_assured_accident_insurance_",
-			"accident_insurance_no", "is_esic_applicable", "variable_end_date", "variable_start_date",
-			"variable_in"
+			"accident_insurance_no", "is_esic_applicable" 
 			]
 
 		if not self.is_new_employee:
