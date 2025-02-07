82d25752f79289640bc9f13e7477c0ef0249db93
Gurukrupa-Export <129185339+Gurukrupa-Export@users.noreply.github.com>
2025-02-07 17:45:56 +0530

    Delete gke_customization/gke_hrms/doctype/overtime_request directory

--- gke_customization/gke_hrms/doctype/overtime_request/test_overtime_request.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Gurukrupa Export and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestOvertimeRequest(FrappeTestCase):
-	pass
