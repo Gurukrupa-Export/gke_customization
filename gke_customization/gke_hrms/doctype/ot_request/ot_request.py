4000b4c0391a160f64cefab44cd2b33c7a20ed6c
prachi-gurukrupa <prachi_b@gkexport.com>
2025-02-08 12:07:36 +0530

    added a filter for active employee and based on workflow state in the child table status will be getting changed

--- gke_customization/gke_hrms/doctype/ot_request/ot_request.py
+++ gke_customization/gke_hrms/doctype/ot_request/ot_request.py
@@ -6,10 +6,24 @@ from frappe.model.document import Document
 
 
 class OTRequest(Document):
-	pass
+	def before_save(self):
+		if self.workflow_state == 'Send For Approval':
+			for row in self.hr_approver:
+				row.status = self.workflow_state
+
+	def before_submit(self):
+		if self.workflow_state == 'Approved':
+			for row in self.hr_approver:
+				row.status = self.workflow_state
+		if self.workflow_state == 'Rejected':
+			for row in self.hr_approver:
+				row.status = self.workflow_state	
+
 
 @frappe.whitelist()
 def fill_employee_details(department):
-	employees = frappe.db.sql(f"""select name,employee_name from `tabEmployee` where department = '{department}' """,as_dict=1)
-	
+	employees = frappe.db.sql(f"""select name, employee_name from `tabEmployee` where department = '{department}' and status = 'Active'""", as_dict=1)
 	return employees
+
+
+	
