c0a174c22d02f17fc3008415142f4fd69a21af8e
Dharm Vaghasiya <dharm_v@gkexport.com>
2025-04-05 09:10:42 +0530

    Update in USL and  Add new Doctype Bussiness Patner
    Update in Code Of USL. Js,Py,JSON and add New Doctype For USL Bussiness Patner

--- gke_customization/gke_custom_export/doctype/unsecured_loan/unsecured_loan.py
+++ gke_customization/gke_custom_export/doctype/unsecured_loan/unsecured_loan.py
@@ -42,8 +42,8 @@ class UnsecuredLoan(Document):
 				if self.repayment_schedule[-1].repay_loan_amount > self.repayment_schedule[-2].balance_loan_amount:
 					frappe.throw("Repayment Loan Amount is not correct")
 
-		if self.workflow_state == 'Interest Accrual':
-			create_gl_entry(self)
+		# if self.workflow_state == 'Interest Accrual':
+		# 	create_gl_entry(self)
 
 
 
@@ -175,7 +175,7 @@ def create_gl_entry(self):
 	if not self.parent_gl_entry_created:
 		credit_entry = frappe.get_doc({
 					"doctype": "GL Entry",
-					"account": "Unsecured Loan from Related Parties - GEPL",
+					"account": self.principal_gl_account,
 					"posting_date": self.loan_date,
 					"credit": self.loan_amount,
 					"credit_in_account_currency": self.loan_amount,
@@ -183,10 +183,28 @@ def create_gl_entry(self):
 					"company": self.company,
 					"voucher_type": "Unsecured Loan",
 					"voucher_no": self.name,
-					"party_type":"Supplier",
-					"party":self.name,
+					"party_type":"Customer",
+					"party":frappe.db.get_value("Business Partner", self.lender, "customer"),
+					"cost_center":self.cost_center,
 				})
 		credit_entry.insert()
+
+		debit_entry = frappe.get_doc({
+					"doctype": "GL Entry",
+					"account": frappe.db.get_value("Business Partner", self.lender, "account"),
+					"posting_date": self.loan_date,
+					"debit": self.loan_amount,
+					"debit_in_account_currency": self.loan_amount,
+					"debit_in_transaction_currency": self.loan_amount,
+					"company": self.company,
+					"voucher_type": "Unsecured Loan",
+					"voucher_no": self.name,
+					"party_type":"Business Partner",
+					"party":self.lender,
+					"cost_center":self.cost_center,
+				})
+		debit_entry.insert()
+
 		frappe.db.commit()
 		self.parent_gl_entry_created = 1
 
@@ -195,22 +213,134 @@ def create_gl_entry(self):
 		if not schedule_row.gl_entry_created:
 			debit_entry = frappe.get_doc({
 					"doctype": "GL Entry",
-					"account": "FACTORY - 7291 - HDFC - GEPL",
+					"account": self.principal_gl_account,
 					"posting_date": schedule_row.payment_date,
-					"debit": schedule_row.total_payment,
-					"debit_in_account_currency": schedule_row.total_payment,
-					"debit_in_transaction_currency": schedule_row.total_payment,
+					"debit": schedule_row.repay_loan_amount,
+					"debit_in_account_currency": schedule_row.repay_loan_amount,
+					"debit_in_transaction_currency": schedule_row.repay_loan_amount,
 					"company": self.company,
 					"voucher_type": "Unsecured Loan",
 					"voucher_no": self.name,
 					"party_type":"Supplier",
-					"party":self.name,
+					"party":frappe.db.get_value("Business Partner", self.lender, "supplier"),
+					"cost_center":self.cost_center,
 					"remarks": f"Debit GL entry for repayment schedule row {schedule_row.idx}",
 				})
 			debit_entry.insert()
+
+			debit_entry = frappe.get_doc({
+					"doctype": "GL Entry",
+					"account": self.interest_gl_account,
+					"posting_date": schedule_row.payment_date,
+					"debit": schedule_row.interest_amount,
+					"debit_in_account_currency": schedule_row.interest_amount,
+					"debit_in_transaction_currency": schedule_row.interest_amount,
+					"company": self.company,
+					"voucher_type": "Unsecured Loan",
+					"voucher_no": self.name,
+					"party_type":"Supplier",
+					"party":frappe.db.get_value("Business Partner", self.lender, "supplier"),
+					"cost_center":self.cost_center,
+					"remarks": f"Debit GL entry for repayment schedule row {schedule_row.idx}",
+				})
+			debit_entry.insert()
+
+			credit_entry = frappe.get_doc({
+					"doctype": "GL Entry",
+					"account": frappe.db.get_value("Business Partner", self.lender, "account"),
+					"posting_date": schedule_row.payment_date,
+					"credit": schedule_row.total_payment,
+					"credit_in_account_currency": schedule_row.total_payment,
+					"credit_in_transaction_currency": schedule_row.total_payment,
+					"company": self.company,
+					"voucher_type": "Unsecured Loan",
+					"voucher_no": self.name,
+					"party_type":"Business Partner",
+					"party":self.lender,
+					"cost_center":self.cost_center,
+				})
+			credit_entry.insert()
+	
 			frappe.db.commit()
 			schedule_row.gl_entry_created = 1
 
 	# return
+
+import frappe
+from frappe.model.document import Document
+
+@frappe.whitelist()
+def create_receive_payment_entry(loan_name):
+	try:
+		loan = frappe.get_doc("Unsecured Loan", loan_name)  # Assuming your Doctype is 'Unsecured Loan'
+		# Fetch Business Partner details
+		business_partner = frappe.get_value("Business Partner", loan.lender, ["customer", "account"], as_dict=True)
+		if not business_partner or not business_partner.get("customer") or not business_partner.get("account"):
+			frappe.throw("Business Partner details are missing.")
+		if not loan.parent_gl_entry_created:
+			# Create Payment Entry
+			payment_entry = frappe.get_doc({
+				"doctype": "Payment Entry",
+				"payment_type": "Receive",
+				"party_type": "Customer",
+				"party": business_partner.get("customer"),
+				"posting_date": frappe.utils.today(),
+				"paid_amount": loan.loan_amount,
+				"received_amount":loan.loan_amount,
+				"company": loan.company,
+				"cost_center": loan.cost_center,
+				"mode_of_payment": "Cash",
+				"paid_from": business_partner.get("account"),
+				"paid_to": loan.principal_gl_account,
+				"custom_unsecured_loan":loan.name
+				})
+			
+			payment_entry.insert(ignore_permissions=True)
+			payment_entry.submit()
+			loan.parent_gl_entry_created = 1
+			loan.save()
+			return {"status": "success", "payment_entry": payment_entry.name}
+		else:
+			frappe.throw("Amount Recevied from this Loan")
+	except Exception as e:
+		return {"status": "error", "message": str(e)}
+
+@frappe.whitelist()
+def create_pay_payment_entry(loan_name):
+	try:
+		loan = frappe.get_doc("Unsecured Loan", loan_name)  # Assuming your Doctype is 'Unsecured Loan'
+		# Fetch Business Partner details
+		business_partner = frappe.get_value("Business Partner", loan.lender, ["supplier", "account"], as_dict=True)
+		if not business_partner or not business_partner.get("supplier") or not business_partner.get("account"):
+			frappe.throw("Business Partner details are missing.")
+		for i in loan.repayment_schedule:
+			if i.gl_entry_created:
+				continue
+			# Create Payment Entry
+			payment_entry = frappe.get_doc({
+				"doctype": "Payment Entry",
+				"payment_type": "Pay",
+				"party_type": "Supplier",
+				"party": business_partner.get("supplier"),
+				"posting_date": frappe.utils.today(),
+				"paid_amount": i.total_payment,
+				"received_amount":i.total_payment,
+				"company": loan.company,
+				"cost_center": loan.cost_center,
+				"mode_of_payment": "Cash",
+				"paid_from": loan.principal_gl_account,
+				"paid_to": business_partner.get("account"),
+				"custom_unsecured_loan":loan.name
+				})
+			payment_entry.insert(ignore_permissions=True)
+			payment_entry.submit()
+			i.gl_entry_created = 1
+		loan.save()
+		return {"status": "success", "payment_entry": payment_entry.name}
+	except Exception as e:
+		return {"status": "error", "message": str(e)}
+
+
+
 
 
