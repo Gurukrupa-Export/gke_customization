9c099e1eca26d03be24967390ed35ae1242f4fd0
Gurukrupa-Export <129185339+Gurukrupa-Export@users.noreply.github.com>
2025-04-22 17:30:34 +0530

    Update task.py
    Add Super User For Task

--- gke_customization/gke_order_forms/doc_events/task.py
+++ gke_customization/gke_order_forms/doc_events/task.py
@@ -4,7 +4,9 @@ from frappe import local
 def has_permission(doc, ptype, user):
     if ptype != "read":
         return True  # allow other permissions normally
-
+        
+    if "Task Manager" in frappe.get_roles(user):
+        return True
     # Check if this task is assigned to the current user
     assigned = frappe.db.exists("ToDo", {
         "reference_type": "Task",
