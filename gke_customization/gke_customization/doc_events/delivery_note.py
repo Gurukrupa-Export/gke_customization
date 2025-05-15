b322d74e0233a94461cd8966b7aaedf0096eec7b
Dharm Vaghasiya <dharm_v@gkexport.com>
2025-05-15 09:28:15 +0530

    Update Delivery Note and Sales Invoice
    Add Condition of Company

--- gke_customization/gke_customization/doc_events/delivery_note.py
+++ gke_customization/gke_customization/doc_events/delivery_note.py
@@ -2,16 +2,17 @@ import frappe
 import json 
 
 def validate(self,method=None):
-	for r in self.items :
-		if r.against_sales_order:
-			Diamond_grade=frappe.db.get_value('Sales Order',r.against_sales_order,'items.diamond_grade')
-			Batch_no=frappe.db.get_value('Sales Order',r.against_sales_order,'items.batch_no')
-			r.diamond_grade=Diamond_grade
-			r.batch_no = Batch_no
-        
-		if not r.batch_no and not r.diamond_grade:
-			frappe.throw("Batch no and Diamond grade are mandatory")
-		elif not r.batch_no:
-			frappe.throw("Batch no is mandatory")
-		elif not r.diamond_grade:
-			frappe.throw("Diamond grade is mandatory")
+	if self.company=='Sadguru Diamond':
+		for r in self.items :
+			if r.against_sales_order:
+				Diamond_grade=frappe.db.get_value('Sales Order',r.against_sales_order,'items.diamond_grade')
+				Batch_no=frappe.db.get_value('Sales Order',r.against_sales_order,'items.batch_no')
+				r.diamond_grade=Diamond_grade
+				r.batch_no = Batch_no
+			
+			if not r.batch_no and not r.diamond_grade:
+				frappe.throw("Batch no and Diamond grade are mandatory")
+			elif not r.batch_no:
+				frappe.throw("Batch no is mandatory")
+			elif not r.diamond_grade:
+				frappe.throw("Diamond grade is mandatory")
--- gke_customization/gke_customization/doc_events/sales_invoice.py
+++ gke_customization/gke_customization/doc_events/sales_invoice.py
@@ -72,17 +72,18 @@ def get_delivery_challan(source_name, target_doc=None):
 
 
 def validate(self,method=None):
-	for r in self.items :
-		if r.sales_order:
-			Diamond_grade=frappe.db.get_value('Sales Order',r.sales_order,'items.diamond_grade')
-			Batch_no=frappe.db.get_value('Sales Order',r.sales_order,'items.batch_no')
-			self.diamond_grade=Diamond_grade
-			self.batch_no = Batch_no
-        
-		if not r.batch_no and not r.diamond_grade:
-			frappe.throw("Batch no and Diamond grade are mandatory")
-		elif not r.batch_no:
-			frappe.throw("Batch no is mandatory")
-		elif not r.diamond_grade:
-			frappe.throw("Diamond grade is mandatory")
+	if self.company=='Sadguru Diamond':
+		for r in self.items :
+			if r.sales_order:
+				Diamond_grade=frappe.db.get_value('Sales Order',r.sales_order,'items.diamond_grade')
+				Batch_no=frappe.db.get_value('Sales Order',r.sales_order,'items.batch_no')
+				self.diamond_grade=Diamond_grade
+				self.batch_no = Batch_no
+			
+			if not r.batch_no and not r.diamond_grade:
+				frappe.throw("Batch no and Diamond grade are mandatory")
+			elif not r.batch_no:
+				frappe.throw("Batch no is mandatory")
+			elif not r.diamond_grade:
+				frappe.throw("Diamond grade is mandatory")
 
