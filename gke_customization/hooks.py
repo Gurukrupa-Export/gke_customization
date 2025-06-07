0112905507a0b571c72fa6f7051d0918b568f84f
Dharm Vaghasiya <dharm_v@gkexport.com>
2025-06-07 11:07:50 +0530

    Add PMO Files
    Add PMO file in overrides and hooks.py to override logic . This is for shorttime

--- gke_customization/hooks.py
+++ gke_customization/hooks.py
@@ -117,7 +117,8 @@ has_permission = {
 
 override_doctype_class = {
 	"Employee Incentive": "gke_customization.overrides.employee_incentive.CustomEmployeeIncentive",
-	"Employee Checkin": "gke_customization.overrides.employee_checkin.CustomEmployeeCheckin"
+	"Employee Checkin": "gke_customization.overrides.employee_checkin.CustomEmployeeCheckin",
+    "Parent Manufacturing Order": "gke_customization.overrides.parent_manufacturing_order.CustomParentManufacturingOrder"
 }
 
 # Document Events
