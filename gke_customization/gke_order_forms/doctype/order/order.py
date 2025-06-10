846dba587711f4d5bc3baab6eb4561fb8eae9a24
Dharm Vaghasiya <160316964+dharm-gkexport@users.noreply.github.com>
2025-06-10 15:07:17 +0530

    Update order.py

--- gke_customization/gke_order_forms/doctype/order/order.py
+++ gke_customization/gke_order_forms/doctype/order/order.py
@@ -1,5 +1,6 @@
 # Copyright (c) 2023, Nirali and contributors
 # For license information, please see license.txt
+# check push
 from __future__ import unicode_literals
 from frappe.utils import flt
 import frappe
