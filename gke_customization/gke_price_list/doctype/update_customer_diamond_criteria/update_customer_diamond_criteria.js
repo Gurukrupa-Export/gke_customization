69eb7f0fdb2a6cf36f25e3ec99f5f379e5edec71
Dharm Vaghasiya <160316964+dharm-gkexport@users.noreply.github.com>
2025-07-07 13:51:11 +0530

    Update update_customer_diamond_criteria.js

--- gke_customization/gke_price_list/doctype/update_customer_diamond_criteria/update_customer_diamond_criteria.js
+++ gke_customization/gke_price_list/doctype/update_customer_diamond_criteria/update_customer_diamond_criteria.js
@@ -1,33 +1,33 @@
 
 
 frappe.ui.form.on('Update Customer Diamond Criteria', {
-    customer: function (frm) {
-        if (!frm.doc.customer) return;
-         frappe.call({
-            method:"gke_customization.gke_price_list.doctype.update_customer_diamond_criteria.update_customer_diamond_criteria.get_customer_diamond_criteria",
-            args: {
-                customer: frm.doc.customer
-            },
-            callback: function (r) {
-                if (!r.message || !r.message.diamond_grades || !r.message.diamond_grades.length) {
-                    frappe.msgprint(__('No diamond grades found for this customer.'));
-                    return;
-                }
-                frm.clear_table('diamond_grades');
+    // customer: function (frm) {
+        // if (!frm.doc.customer) return;
+        //  frappe.call({
+        //     method:"gke_customization.gke_price_list.doctype.update_customer_diamond_criteria.update_customer_diamond_criteria.get_customer_diamond_criteria",
+        //     args: {
+        //         customer: frm.doc.customer
+        //     },
+        //     callback: function (r) {
+        //         if (!r.message || !r.message.diamond_grades || !r.message.diamond_grades.length) {
+        //             frappe.msgprint(__('No diamond grades found for this customer.'));
+        //             return;
+        //         }
+        //         frm.clear_table('diamond_grades');
 
-                r.message.diamond_grades.forEach(row => {
-                    let child = frm.add_child('diamond_grades');
-                    child.diamond_quality = row.diamond_quality;
-                    child.diamond_grade_1 = row.diamond_grade_1;
-                    child.diamond_grade_2 = row.diamond_grade_2;
-                    child.diamond_grade_3 = row.diamond_grade_3;
-                    child.diamond_grade_4 = row.diamond_grade_4;
-                });
+        //         r.message.diamond_grades.forEach(row => {
+        //             let child = frm.add_child('diamond_grades');
+        //             child.diamond_quality = row.diamond_quality;
+        //             child.diamond_grade_1 = row.diamond_grade_1;
+        //             child.diamond_grade_2 = row.diamond_grade_2;
+        //             child.diamond_grade_3 = row.diamond_grade_3;
+        //             child.diamond_grade_4 = row.diamond_grade_4;
+        //         });
 
-                frm.refresh_field('diamond_grades');
-            }
-        });
-    },
+        //         frm.refresh_field('diamond_grades');
+        //     }
+        // });
+    // },
    //  setup(frm) {
    //      frm.set_query('diamond_quality', 'diamond_grades', function () {
    //          return {
