b6f13b087e1022c7b5f4f7dfb4842d01623e6efc
Dharm Vaghasiya <160316964+dharm-gkexport@users.noreply.github.com>
2025-07-10 16:48:04 +0530

    Delete gke_customization/gke_custom_export/doctype/update_customer_diamond_criteria directory

--- gke_customization/gke_custom_export/doctype/update_customer_diamond_criteria/test_update_customer_diamond_criteria.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Gurukrupa Export and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestUpdateCustomerDiamondCriteria(FrappeTestCase):
-	pass
