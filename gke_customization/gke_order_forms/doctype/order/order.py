d5f85ca7c154978ac585f7a8b550977e0d1114a6
Dharm Vaghasiya <160316964+dharm-gkexport@users.noreply.github.com>
2025-07-10 18:04:21 +0530

    Update order.py
    Add Function to validate Duplicate variant

--- gke_customization/gke_order_forms/doctype/order/order.py
+++ gke_customization/gke_order_forms/doctype/order/order.py
@@ -891,6 +891,17 @@ def create_line_items(self):
 		
 	elif self.item_type == 'Only Variant':
 		design_id = frappe.db.get_value('Order', self.name, 'design_id')
+		template = frappe.db.get_value("Item",design_id,"variant_of")
+		args = make_atribute_list(self.name)
+		variant_item_code = None
+		possible_variants = [i for i in get_item_codes_by_attributes(args, template) if i != variant_item_code]
+		for variant in possible_variants:
+			variant = frappe.get_doc("Item", variant)
+			if variant:
+				final_variant = str(variant).replace("Item(","").replace(")","")
+				frappe.throw(f"Already available {final_variant}")
+
+		
 		item_variant = create_only_variant_from_order(self, self.name)
 
 		frappe.db.set_value('Item', item_variant[0], {
@@ -931,6 +942,65 @@ def create_line_items(self):
 	frappe.db.set_value(self.doctype, self.name, "item_remark", "New Item" if new_item_created else "Copy Paste Item")
 
 	return item_variant
+
+def get_item_codes_by_attributes(attribute_filters, template_item_code=None):
+	items = []
+
+	for attribute, values in attribute_filters.items():
+		attribute_values = values
+
+		if not isinstance(attribute_values, list):
+			attribute_values = [attribute_values]
+
+		if not attribute_values:
+			continue
+
+		wheres = []
+		query_values = []
+		for attribute_value in attribute_values:
+			wheres.append("( attribute = %s and attribute_value = %s )")
+			query_values += [attribute, attribute_value]
+
+		attribute_query = " or ".join(wheres)
+
+		if template_item_code:
+			variant_of_query = "AND t2.variant_of = %s"
+			query_values.append(template_item_code)
+		else:
+			variant_of_query = ""
+
+		query = f"""
+			SELECT
+				t1.parent
+			FROM
+				`tabItem Variant Attribute` t1
+			WHERE
+				1 = 1
+				AND (
+					{attribute_query}
+				)
+				AND EXISTS (
+					SELECT
+						1
+					FROM
+						`tabItem` t2
+					WHERE
+						t2.name = t1.parent
+						{variant_of_query}
+				)
+			GROUP BY
+				t1.parent
+			ORDER BY
+				NULL
+		"""
+
+		item_codes = set([r[0] for r in frappe.db.sql(query, query_values)])
+		items.append(item_codes)
+
+	res = list(set.intersection(*items))
+
+	return res
+
 
 
 
