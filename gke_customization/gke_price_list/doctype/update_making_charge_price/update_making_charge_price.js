1bfe785294016738d66872f7c5f836970df7a61e
Dharm Vaghasiya <160316964+dharm-gkexport@users.noreply.github.com>
2025-07-17 14:29:48 +0530

    Delete gke_customization/gke_price_list/doctype/update_making_charge_price directory

--- gke_customization/gke_price_list/doctype/update_making_charge_price/update_making_charge_price.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Gurukrupa Export and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Update Making Charge Price', {
-	// refresh: function(frm) {
-
-	// }
-});
