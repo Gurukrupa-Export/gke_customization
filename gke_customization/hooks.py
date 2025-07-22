6a5fc79ee990b689315523bc4e7e2e568aaa8149
Dharm Vaghasiya <160316964+dharm-gkexport@users.noreply.github.com>
2025-07-22 14:48:48 +0530

    Update hooks.py
    Add Payment Entry on submit method in hooks.py file

--- gke_customization/hooks.py
+++ gke_customization/hooks.py
@@ -259,9 +259,10 @@ doc_events = {
 # "Shareholder": {
 #     "validate": "gke_customization.gke_order_forms.doc_events.shareholder.validate"
 # },
-# "Payment Entry": {
-#     "on_update_after_submit": "gke_customization.gke_order_forms.doc_events.payment_entry.on_update_after_submit"
-# },
+"Payment Entry": {
+    # "on_update_after_submit": "gke_customization.gke_order_forms.doc_events.payment_entry.on_update_after_submit"
+    "on_submit": "gke_customization.gke_order_forms.doc_events.payment_entry.on_submit"
+},
 "Journal Entry": {
     "on_submit": "gke_customization.gke_order_forms.doc_events.journal_entry.on_submit"
 },
