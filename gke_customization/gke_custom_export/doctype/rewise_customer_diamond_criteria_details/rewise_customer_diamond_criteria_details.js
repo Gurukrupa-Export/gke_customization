de3fecb9eeacc8c3a514ca0bca77e411d867718a
Dharm Vaghasiya <160316964+dharm-gkexport@users.noreply.github.com>
2025-07-24 09:24:18 +0530

    Delete gke_customization/gke_custom_export/doctype/rewise_customer_diamond_criteria_details directory

--- gke_customization/gke_custom_export/doctype/rewise_customer_diamond_criteria_details/rewise_customer_diamond_criteria_details.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Gurukrupa Export and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Rewise Customer Diamond Criteria Details", {
-// 	refresh(frm) {
-
-// 	},
-// });
