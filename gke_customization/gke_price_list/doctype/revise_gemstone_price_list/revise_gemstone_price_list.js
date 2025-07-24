d018caaf10947545d7640c986ac57f88f4f8589f
Dharm Vaghasiya <dharm_v@gkexport.com>
2025-07-24 09:40:26 +0530

    CHnage all doctype name from Rewise to Revise
    Change all doctype name from "Rewise" to "Revise"
    Revise Diamond Price List and  3 tables.
    Revise Gemstone Price List and 1 table..
    Revise Making Charge List and 2 table..
    Revise Fields.
    Revise Customer Diamond Criteria Details and 1 table.

---
+++ gke_customization/gke_price_list/doctype/revise_gemstone_price_list/revise_gemstone_price_list.js
@@ -0,0 +1,37 @@
+// Copyright (c) 2024, Gurukrupa Export and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Revise Gemstone Price List', {
+	onload: function(frm) {
+		if(frm.doc.name){
+			frappe.call({
+				method: 'gke_customization.gke_price_list.doctype.revise_gemstone_price_list.revise_gemstone_price_list.get_value',
+				args: {
+					'doc': frm.doc,
+				},
+				callback: function(r) {
+					if (!r.exc) {
+						// code snippet
+					}
+				}
+			});
+		}
+	},
+	revised_rate: function(frm){
+		frm.set_value('difference',frm.doc.revised_rate-frm.doc.rate_per_carat)
+	}
+});
+
+
+frappe.ui.form.on('Revise Gemstone Price List Details', {
+	revised_rate:function(frm,cdt,cdn) {
+		var d = locals[cdt][cdn];
+		d.difference = d.revised_rate - d.rate_per_carat
+		refresh_field('update_gemstone_price_list_details')
+	},
+	revised_gemstone_handling_rate:function(frm,cdt,cdn) {
+		var d = locals[cdt][cdn];
+		d.difference_gemstone_handling_rate = d.revised_gemstone_handling_rate - d.gemstone_handling_rate
+		refresh_field('update_gemstone_price_list_details')
+	},
+})
