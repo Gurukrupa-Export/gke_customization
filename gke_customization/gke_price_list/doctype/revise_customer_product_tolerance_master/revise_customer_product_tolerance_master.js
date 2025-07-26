924a64a39947fb10e4f61b110030a39f665fcf5e
Dharm Vaghasiya <dharm_v@gkexport.com>
2025-07-26 13:08:26 +0530

    Add Doctypes for Revise Customer Product Tolerance
    Add Doctypes for Revise Customer Product Tolerance. Add 3 Child Table

---
+++ gke_customization/gke_price_list/doctype/revise_customer_product_tolerance_master/revise_customer_product_tolerance_master.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Gurukrupa Export and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Revise Customer Product Tolerance Master", {
+// 	refresh(frm) {
+
+// 	},
+// });
