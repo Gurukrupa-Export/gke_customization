869530fbc7889900ab02597fa7a5857cf1e3a99c
Dharm Vaghasiya <dharm_v@gkexport.com>
2025-07-28 11:27:15 +0530

    Update Bhavika's Development
    Update Employee Onboarding public js and py file

--- gke_customization/public/js/doctype_js/employee_onboarding.js
+++ gke_customization/public/js/doctype_js/employee_onboarding.js
@@ -2,21 +2,20 @@
 frappe.ui.form.on("Employee Onboarding", {
     refresh: function (frm) {
         if (!frm.doc.employee && frm.doc.docstatus === 1) {
-            setTimeout(()=>{
+            // setTimeout(()=>{
                 frm.remove_custom_button("Employee", "Create")
                 frm.add_custom_button(
                     __("Employee Update"),
                     function () {
                         frappe.model.open_mapped_doc({
-                            method: "gke_customization.gke_hrms.override.employee_onboarding.employee_update",
+                            method: "gke_customization.gke_hrms.doc_events.employee_onboarding.employee_update",
                             frm: frm,
                         });
                     },
                     __("Create")
                 );
                 frm.page.set_inner_btn_group_as_primary(__("Create"));
-            }, 100)
-            
+            // }, 100)           
             
         }
     },
