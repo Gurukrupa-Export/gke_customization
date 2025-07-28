9b99656f1aac2ebdeaf8d27a63f23e8e058cc019
prachi-gurukrupa <prachi_b@gkexport.com>
2025-07-28 12:18:15 +0530

    Rewise filed changes for 0 value
    Rewise filed changes for 0 value

--- gke_customization/gke_order_forms/doctype/revise_field/revise_field.js
+++ gke_customization/gke_order_forms/doctype/revise_field/revise_field.js
@@ -41,7 +41,7 @@ frappe.ui.form.on("Revise Field", {
 						"Back Belt Length": "back_belt_length"
 					};
 
-					let fields_updated = false;
+					let values_to_set = {};
 
 					for (let [attribute, value] of Object.entries(attribute_map)) {
 						let fieldname = attribute_to_field[attribute];
@@ -49,7 +49,8 @@ frappe.ui.form.on("Revise Field", {
 						if (fieldname && frm.fields_dict[fieldname]) {
 							let current_val = frm.doc[fieldname];
 
-							if (current_val !== value) {
+							// Use string comparison to avoid mismatch due to types
+							if (String(current_val ?? "") !== String(value ?? "")) {
 								let df = frm.fields_dict[fieldname].df;
 
 								if (df.fieldtype === "Select" && df.options && !df.options.includes(value)) {
@@ -57,15 +58,13 @@ frappe.ui.form.on("Revise Field", {
 									frm.refresh_field(fieldname);
 								}
 
-								frm.set_value(fieldname, value);
-								fields_updated = true;
+								values_to_set[fieldname] = value;
 							}
 						}
 					}
 
-					// Optional: set a hidden field or flag to avoid repeating update on load
-					if (!fields_updated) {
-						frm.doc.__attribute_applied = true;
+					if (Object.keys(values_to_set).length > 0) {
+						frm.set_value(values_to_set);
 					}
 				}
 			}
@@ -73,9 +72,62 @@ frappe.ui.form.on("Revise Field", {
 	},
 
 	onload: function(frm) {
-		// Prevent re-triggering if already applied
-		if (frm.doc.item && !frm.doc.__attribute_applied) {
-			frm.trigger("item");
+		// Safe load trigger
+		if (frm.doc.item) {
+			frappe.call({
+				method: "gke_customization.gke_order_forms.doctype.revise_field.revise_field.get_filtered_item_attributes",
+				args: {
+					item_code: frm.doc.item
+				},
+				callback: function(r) {
+					if (r.message) {
+						const attribute_map = r.message;
+						let attribute_to_field = {
+							"Metal Colour": "metal_colour",
+							"Diamond Target": "diamond_target",
+							"Stone Changeable": "stone_changeable",
+							"Gemstone Type": "gemstone_type",
+							"Chain Type": "chain_type",
+							"Chain Length": "chain_length",
+							"Feature": "feature",
+							"Rhodium": "rhodium",
+							"Enamal": "enamal",
+							"Detachable": "detachable",
+							"Cap/Ganthan": "capganthan",
+							"Two in One": "two_in_one",
+							"Product Size": "product_size",
+							"Sizer Type": "sizer_type",
+							"Lock Type": "lock_type",
+							"Black Bead Line": "black_bead_line",
+							"Charm": "charm",
+							"Count of Spiral Turns": "count_of_spiral_turns",
+							"Number of Ant": "number_of_ant",
+							"Back Side Size": "back_side_size",
+							"Space between Mugappu": "space_between_mugappu",
+							"Distance Between Kadi To Mugappu": "distance_between_kadi_to_mugappu",
+							"Back Belt": "back_belt",
+							"Back Belt Length": "back_belt_length"
+						};
+
+						let values_to_set = {};
+
+						for (let [attribute, value] of Object.entries(attribute_map)) {
+							let fieldname = attribute_to_field[attribute];
+							if (fieldname && frm.fields_dict[fieldname]) {
+								let current_val = frm.doc[fieldname];
+
+								if (String(current_val ?? "") !== String(value ?? "")) {
+									values_to_set[fieldname] = value;
+								}
+							}
+						}
+
+						if (Object.keys(values_to_set).length > 0) {
+							frm.set_value(values_to_set);
+						}
+					}
+				}
+			});
 		}
 	}
 });
