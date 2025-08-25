329ffa5319782619c4956060046aa0954443132d
bhavika-gkexport <bhavika_p@gkexport.com>
2025-08-25 11:37:35 +0530

    25-08-2025 Update in Employee Exit Questionnaire doctype's json file
    25-08-2025
    Update in Employee Exit Questionnaire doctype's json file

--- gke_customization/gke_hrms/doctype/employee_exit_questionnaire/employee_exit_questionnaire.py
+++ gke_customization/gke_hrms/doctype/employee_exit_questionnaire/employee_exit_questionnaire.py
@@ -1,8 +1,9 @@
-# Copyright (c) 2023, Gurukrupa Export and contributors
+# Copyright (c) 2025, Gurukrupa Export and contributors
 # For license information, please see license.txt
 
 # import frappe
 from frappe.model.document import Document
+
 
 class EmployeeExitQuestionnaire(Document):
 	pass
