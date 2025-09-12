c7c6de7f3b3986372700d43b56dba78a9d13f77a
Shubham-gke <shubham_s@gkexport.com>
2025-09-12 18:38:23 +0530

    12-09-2025 added one method in ovverride
    12-09-2025 i created one override method validate_account_party_type
    and remove the monkey patching from the payment_entry.py

--- gke_customization/__init__.py
+++ gke_customization/__init__.py
@@ -1,3 +1,7 @@
 
 __version__ = '0.0.1'
 
+import erpnext.accounts.party as party_module
+from gke_customization.overrides.validate_party import  gke_custom_validate_account_party_type
+
+party_module.validate_account_party_type =  gke_custom_validate_account_party_type
