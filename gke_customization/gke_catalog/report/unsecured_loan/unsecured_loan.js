50f91a5ae29adfe03d9580b9630d755cedf825f0
sahilgiri_gk <girisahil2000@gmail.com>
2025-09-19 12:13:10 +0530

    Update unsecured_loan.js

--- gke_customization/gke_catalog/report/unsecured_loan/unsecured_loan.js
+++ gke_customization/gke_catalog/report/unsecured_loan/unsecured_loan.js
@@ -1,30 +1,66 @@
-frappe.query_reports["Lender Loan Repayment Summary"] = {
+// Copyright (c) 2025, Gurukrupa Export and contributors
+// For license information, please see license.txt
+
+
+frappe.query_reports["Unsecure Loan Detail Report"] = {
     "filters": [
-		{
-            "fieldname": "name",
-            "label": "Unsecured Loan ID",
+        {
+            "fieldname": "company",
+            "label": __("Company"),
             "fieldtype": "Link",
-            "options": "Unsecured Loan",
-            "default": "",
+            "options": "Company",
+            "default": frappe.defaults.get_user_default("Company"),
+            "reqd": 1
+        },
+        {
+            "fieldname": "branch",
+            "label": __("Branch"),
+            "fieldtype": "Link",
+            "options": "Branch",
+            "get_query": function() {
+                var company = frappe.query_report.get_filter_value("company");
+                if (company) {
+                    return { 
+                        filters: { 
+                            company: company 
+                        } 
+                    };
+                }
+            }
         },
         {
             "fieldname": "lender",
-            "label": "Lender",
+            "label": __("Lender"),
             "fieldtype": "Link",
-            "options": "Business Partner",  // or "Lender" if custom Doctype
-            "reqd": 1
+            "options": "Business Partner"
         },
-		{
+        {
+            "fieldname": "name",
+            "label": __("Unsecured Loan ID"),
+            "fieldtype": "Link",
+            "options": "Unsecured Loan",
+            "get_query": function() {
+                var company = frappe.query_report.get_filter_value("company");
+                var branch = frappe.query_report.get_filter_value("branch");
+                var lender = frappe.query_report.get_filter_value("lender");
+                
+                var filters = { docstatus: ["!=", 2] };
+                if (company) filters.company = company;
+                if (branch) filters.branch = branch;
+                if (lender) filters.lender = lender;
+                
+                return { filters: filters };
+            }
+        },
+        {
             "fieldname": "from_date",
-            "label": "From Date",
-            "fieldtype": "Date",
-            "default": frappe.datetime.add_months(frappe.datetime.get_today(), -1)
+            "label": __("From Date"),
+            "fieldtype": "Date"
         },
         {
             "fieldname": "to_date",
-            "label": "To Date",
-            "fieldtype": "Date",
-            "default": frappe.datetime.get_today()
+            "label": __("To Date"),
+            "fieldtype": "Date"
         }
     ]
 };
