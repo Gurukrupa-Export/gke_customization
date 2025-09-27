fe3d2309e0e68f28afca98281e4d2f7435f73567
Dharm Vaghasiya <dharm_v@gkexport.com>
2025-09-27 10:39:20 +0530

    Add Sahil Report "Pending Order Form Summary"
    Add Sahil Report "Pending Order Form Summary"

---
+++ gke_customization/gke_catalog/report/pending_order_form_summary/pending_order_form_summary.js
@@ -0,0 +1,30 @@
+// Copyright (c) 2025, Gurukrupa Export and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Pending Order Form Summary"] = {
+    "filters": [
+        {
+            "fieldname": "company",
+            "label": __("Company"),
+            "fieldtype": "Select",
+            "options": "\nGurukrupa Export Private Limited\nKG GK Jewellers Private Limited",
+            "reqd": 0
+        },
+        {
+            "fieldname": "branch",
+            "label": __("Branch"),
+            "fieldtype": "Link",
+            "options": "Branch",
+            "reqd": 0
+        },
+        {
+            "fieldname": "order_form_no",
+            "label": __("Order Form No."),
+            "fieldtype": "MultiSelectList",
+            "get_data": function(txt) {
+                return frappe.db.get_link_options('Order Form', txt);
+            },
+            "reqd": 0
+        }
+    ]
+};
