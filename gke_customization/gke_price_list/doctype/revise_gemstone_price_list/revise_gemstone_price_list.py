43ea3050c363ae76b8f5ccc30000a93ab5eec118
Shruti Bansal <shruti_b@gkexport.com>
2025-12-01 16:36:28 +0530

    Updates In Revise Gemstone Price List
    Updates In Revise Gemstone Price List

--- gke_customization/gke_price_list/doctype/revise_gemstone_price_list/revise_gemstone_price_list.py
+++ gke_customization/gke_price_list/doctype/revise_gemstone_price_list/revise_gemstone_price_list.py
@@ -16,44 +16,44 @@ class ReviseGemstonePriceList(Document):
                 "gemstone_quality": self.stone_quality,
 			}
                    
-        if self.price_list_type == 'Weight (in cts)':
-            if len(self.revise_gemstone_price_list_details) != len(frappe.db.get_list("Gemstone Price List",filters=filters,fields=["from_weight","to_weight"])):
-                if self.revise_gemstone_price_list_details==[]:
-                    self.set("revise_gemstone_price_list_details", [])
-                    # old_diamond_price_list = frappe.db.get_list("Gemstone Price List",filters=filters,fields=["from_weight","to_weight"])
-                    old_gemstone_price_list = frappe.db.get_list("Gemstone Price List",filters=filters,fields=["from_weight","to_weight"])
-                    gemstone_price_list = []
-                    for j in old_gemstone_price_list:
-                        gemstone_price_list.append({"weight_range":f"{j['from_weight']}-{j['to_weight']}"})
+        # if self.price_list_type == 'Weight (in cts)':
+        #     if len(self.revise_gemstone_price_list_details) != len(frappe.db.get_list("Gemstone Price List",filters=filters,fields=["from_weight","to_weight"])):
+        #         if self.revise_gemstone_price_list_details==[]:
+        #             self.set("revise_gemstone_price_list_details", [])
+        #             # old_diamond_price_list = frappe.db.get_list("Gemstone Price List",filters=filters,fields=["from_weight","to_weight"])
+        #             old_gemstone_price_list = frappe.db.get_list("Gemstone Price List",filters=filters,fields=["from_weight","to_weight"])
+        #             gemstone_price_list = []
+        #             for j in old_gemstone_price_list:
+        #                 gemstone_price_list.append({"weight_range":f"{j['from_weight']}-{j['to_weight']}"})
 
-                    output_list = remove_duplicate_data(gemstone_price_list)
-                    sorted_data = sort_data(self,output_list)
-                    set_data_in_child_table(self,sorted_data)
-                else:
-                    old_gemstone_price_list = [d.gemstone_price_list for d in self.revise_gemstone_price_list_details]
-                    for j in frappe.db.get_list("Gemstone Price List",filters=filters,pluck="name"):
-                        if j not in old_gemstone_price_list:
-                            sorted_data = []
-                            for k in frappe.db.get_list("Gemstone Price List",filters={"name":j},fields=["from_weight","to_weight"]):
-                                sorted_data.append({"weight_range":f"{k['from_weight']}-{k['to_weight']}"})
-                            set_data_in_child_table(self,sorted_data)
-        else:
-            old_rate = frappe.db.get_list("Gemstone Price List",filters=filters,fields=["rate","name"])
-            self.rate_per_carat = old_rate[0]['rate']
-            self.gemstone_price_list = old_rate[0]['name']
+        #             output_list = remove_duplicate_data(gemstone_price_list)
+        #             sorted_data = sort_data(self,output_list)
+        #             set_data_in_child_table(self,sorted_data)
+        #         else:
+        #             old_gemstone_price_list = [d.gemstone_price_list for d in self.revise_gemstone_price_list_details]
+        #             for j in frappe.db.get_list("Gemstone Price List",filters=filters,pluck="name"):
+        #                 if j not in old_gemstone_price_list:
+        #                     sorted_data = []
+        #                     for k in frappe.db.get_list("Gemstone Price List",filters={"name":j},fields=["from_weight","to_weight"]):
+        #                         sorted_data.append({"weight_range":f"{k['from_weight']}-{k['to_weight']}"})
+        #                     set_data_in_child_table(self,sorted_data)
+        # else:
+        old_rate = frappe.db.get_list("Gemstone Price List",filters=filters,fields=["rate","name"])
+        self.rate_per_carat = old_rate[0]['rate']
+        self.gemstone_price_list = old_rate[0]['name']
             
                             
     def on_submit(self):
-        if self.price_list_type == 'Weight (in cts)':
-            for i in self.revise_gemstone_price_list_details:
-                # if i.difference!=0:
-                frappe.db.set_value('Gemstone Price List',i.gemstone_price_list,{'rate':i.revised_rate,'effective_from':self.date})
-                if not i.gemstone_price_list:
-                    crate_price_list(self,i)
-        else:
-            frappe.db.set_value('Gemstone Price List',self.gemstone_price_list,{'rate':self.revised_rate,'effective_from':self.date})
-            if not self.gemstone_price_list:
-                crate_price_list(self,i)
+        # if self.price_list_type == 'Weight (in cts)':
+        #     for i in self.revise_gemstone_price_list_details:
+        #         # if i.difference!=0:
+        #         frappe.db.set_value('Gemstone Price List',i.gemstone_price_list,{'rate':i.revised_rate,'effective_from':self.date})
+        #         if not i.gemstone_price_list:
+        #             crate_price_list(self,i)
+        # else:
+        frappe.db.set_value('Gemstone Price List',self.gemstone_price_list,{'rate':self.revised_rate,'effective_from':self.date})
+        if not self.gemstone_price_list:
+            crate_price_list(self,i)
         frappe.msgprint("Price List Updated")
             
 def crate_price_list(self,row):
@@ -76,11 +76,11 @@ def crate_price_list(self,row):
         gemstone_price_list_doc.outwork_handling_charges_rate = self.outwork_handling_charges_rate
 
 
-    if self.price_list_type == 'Weight (in cts)':
-        from_weight = float(row.weight.split('-')[0])
-        to_weight = float(row.weight.split('-')[1])
-        gemstone_price_list_doc.from_weight = from_weight
-        gemstone_price_list_doc.to_weight = to_weight
+    # if self.price_list_type == 'Weight (in cts)':
+    #     from_weight = float(row.weight.split('-')[0])
+    #     to_weight = float(row.weight.split('-')[1])
+    #     gemstone_price_list_doc.from_weight = from_weight
+    #     gemstone_price_list_doc.to_weight = to_weight
 
     gemstone_price_list_doc.effective_from = frappe.utils.now()
     gemstone_price_list_doc.rate = row.revised_rate
@@ -92,11 +92,11 @@ def custom_sort(item):
     start, end = map(float, item['weight_range'][1:].split('-'))
     return (start, end)
 
-def sort_data(self,output_list):
-    if self.price_list_type == 'Weight (in cts)':
-        sorted_data = sorted(output_list, key=custom_sort)
+# def sort_data(self,output_list):
+#     # if self.price_list_type == 'Weight (in cts)':
+#     sorted_data = sorted(output_list, key=custom_sort)
 
-    return sorted_data
+#     return sorted_data
 
 def remove_duplicate_data(gemstone_price_list):
     seen = set()
@@ -108,10 +108,10 @@ def remove_duplicate_data(gemstone_price_list):
             output_list.append(d)
     return output_list
 
-def set_data_in_child_table(self,sorted_data):
-    for i in sorted_data:
-        if self.price_list_type == 'Weight (in cts)':
-            for_weight_in_cts(self,i)
+# def set_data_in_child_table(self,sorted_data):
+#     for i in sorted_data:
+#         if self.price_list_type == 'Weight (in cts)':
+#             for_weight_in_cts(self,i)
 
 def for_weight_in_cts(self,i):
     rate_filters = {
@@ -136,19 +136,19 @@ def for_weight_in_cts(self,i):
     rate_details.weight = i['weight_range']
     
 
-@frappe.whitelist()
-def get_value(doc):
-    json_doc = json.loads(doc)
-    data = []
-    if json_doc['price_list_type'] == 'Weight (in cts)':
-        for i in json_doc['revise_gemstone_price_list_details']:
-            data.append(i['weight'])
+# @frappe.whitelist()
+# def get_value(doc):
+#     json_doc = json.loads(doc)
+#     data = []
+#     if json_doc['price_list_type'] == 'Weight (in cts)':
+#         for i in json_doc['revise_gemstone_price_list_details']:
+#             data.append(i['weight'])
         
-        numeric_ranges = [(float(r.split('-')[0]), float(r.split('-')[1])) for r in data]
-        sorted_ranges = sorted(zip(data, numeric_ranges), key=lambda x: x[1])
-        sorted_ranges = [r[0] for r in sorted_ranges]
-        doc1 = frappe.get_doc('Revise Gemstone Price List',json_doc['name'])
-        for j in doc1.revise_gemstone_price_list_details:
-            frappe.db.set_value('Revise Gemstone Price List Details',j.name,'idx',sorted_ranges.index(j.weight))
+#         numeric_ranges = [(float(r.split('-')[0]), float(r.split('-')[1])) for r in data]
+#         sorted_ranges = sorted(zip(data, numeric_ranges), key=lambda x: x[1])
+#         sorted_ranges = [r[0] for r in sorted_ranges]
+#         doc1 = frappe.get_doc('Revise Gemstone Price List',json_doc['name'])
+#         for j in doc1.revise_gemstone_price_list_details:
+#             frappe.db.set_value('Revise Gemstone Price List Details',j.name,'idx',sorted_ranges.index(j.weight))
 
     
