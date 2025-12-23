17ca600e8a8dfba7e4eef77a680500cc8ceac01a
sahilgiri_gk <girisahil2000@gmail.com>
2025-12-23 15:59:49 +0530

    Update partner_capital_detailed_report.js

--- gke_customization/gke_catalog/report/partner_capital_detailed_report/partner_capital_detailed_report.js
+++ gke_customization/gke_catalog/report/partner_capital_detailed_report/partner_capital_detailed_report.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2025, Gurukrupa Export and contributors
 // For license information, please see license.txt
 
+
 frappe.query_reports["Partner Capital Detailed Report"] = {
     "filters": [
         {
@@ -28,8 +29,8 @@ frappe.query_reports["Partner Capital Detailed Report"] = {
             }
         },
         {
-            "fieldname": "lender",
-            "label": __("Lender"),
+            "fieldname": "business_partner",
+            "label": __("Business Partner"),
             "fieldtype": "Link",
             "options": "Business Partner"
         },
@@ -41,11 +42,15 @@ frappe.query_reports["Partner Capital Detailed Report"] = {
             "get_query": function() {
                 var company = frappe.query_report.get_filter_value("company");
                 var branch = frappe.query_report.get_filter_value("branch");
-                var lender = frappe.query_report.get_filter_value("lender");
+                var business_partner = frappe.query_report.get_filter_value("business_partner");
+
+
                 var filters = { "docstatus": ["!=", 2] };
                 if (company) filters.company = company;
                 if (branch) filters.branch = branch;
-                if (lender) filters.lender = lender;
+                if (business_partner) filters.business_partner = business_partner;
+
+
                 return { "filters": filters };
             }
         },
