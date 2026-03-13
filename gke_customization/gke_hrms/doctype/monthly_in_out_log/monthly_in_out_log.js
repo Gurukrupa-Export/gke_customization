ff8742286e33c3f8ded7312d6c39299d72bba948
aditya-gkexport <aditya_r@gkexport.com>
2026-03-13 15:15:29 +0530

    feat: Refactor data fetching logic in Monthly In-Out Log to handle response and update form fields

--- gke_customization/gke_hrms/doctype/monthly_in_out_log/monthly_in_out_log.js
+++ gke_customization/gke_hrms/doctype/monthly_in_out_log/monthly_in_out_log.js
@@ -5,8 +5,22 @@ frappe.ui.form.on("Monthly In-Out Log", {
 	refresh(frm) {
         if (frm.doc.docstatus == 1) {
             frm.add_custom_button(__("Fetch Latest Data"), function () {
-                frm.call("populate_from_attendance").then(() => {
-                    frm.reload_doc();
+                frm.call({
+                    method : "populate_from_attendance",
+                    doc: frm.doc,
+                    callback: function(r) {
+                        // TODO: Handle the response
+                        const response = r.message || r.docs || [];
+                        console.log(response);
+                        if (response.length > 0) {
+                            doc_data = response[0];
+                            // TODO: Update the form with the response
+                            let filed_name = ["net_wrk_hrs", "spent_hrs", "p_out_hrs", "ot_hrs", "in_time", "out_time", "early_hrs", "late_hrs", "late"];
+                            filed_name.forEach((field) => {
+                                frm.set_value(field, doc_data?.[field] || null);
+                            });
+                        }
+                    }
                 })
             })
         }
