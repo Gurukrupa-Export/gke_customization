4fb8c25ba4f77c2f06dc97ecd3fc46e5e5c95900
sahilgiri_gk <girisahil2000@gmail.com>
2026-03-14 09:20:17 +0530

    Update ot_report.js

--- gke_customization/gke_catalog/report/ot_report/ot_report.js
+++ gke_customization/gke_catalog/report/ot_report/ot_report.js
@@ -1,95 +1,108 @@
-// Copyright (c) 2024, Gurukrupa and contributors
-/* eslint-disable */
-
 frappe.query_reports["OT Report"] = {
+    onload: function (report) {
+        frappe.call({
+            method: "gke_customization.gke_catalog.report.ot_report.ot_report.get_month_range",
+            callback: function (r) {
+                if (r.message) {
+                    let month_field = report.get_filter("month");
+                    if (month_field) {
+                        month_field.df.options = [""].concat(r.message);
+                        month_field.refresh();
+                    }
+                }
+            }
+        });
+    },
+
     filters: [
         {
-            label: __("Month"),
-            fieldtype: "Select",
-            fieldname: "month",
-            reqd: 1,
-            options: [],
-            default: () => {
-                const dateObject = new Date();
-                const options = { month: "short", year: "numeric" };
-                const dateString = dateObject.toLocaleDateString("en-US", options);
-                return dateString;
-            },
-            on_change: function(query_report) {
-                var _month = query_report.get_filter_value('month');
-                if (!_month) return;
-                let firstDayOfMonth = moment(_month, "MMM YYYY").toDate();
-                firstDayOfMonth = frappe.datetime.obj_to_str(firstDayOfMonth);
-                let lastDayOfMonth = frappe.datetime.month_end(firstDayOfMonth);
-                query_report.set_filter_value({
-                    "from_date": firstDayOfMonth,
-                    "to_date":   lastDayOfMonth
-                });
-            }
-        },
-        {
-            label: __("From Date"),
-            fieldtype: "Date",
-            fieldname: "from_date",
-            read_only: 1,
-            default: frappe.datetime.month_start(frappe.datetime.get_today()),
-            on_change: function(query_report) {
-                var from_date = query_report.get_values().from_date;
-                if (!from_date) return;
-                let date    = new moment(from_date);
-                var to_date = date.endOf('month').format();
-                query_report.set_filter_value({ "to_date": to_date });
-            }
-        },
-        {
-            label: __("To Date"),
-            fieldtype: "Date",
-            fieldname: "to_date",
-            reqd: 1,
-            read_only: 1,
-            default: frappe.datetime.month_end(frappe.datetime.get_today())
-        },
-        {
+            fieldname: "company",
             label: __("Company"),
             fieldtype: "Link",
-            fieldname: "company",
             options: "Company",
-            default: frappe.defaults.get_default("company")
         },
         {
+            fieldname: "branch",
             label: __("Branch"),
             fieldtype: "Link",
-            fieldname: "branch",
-            options: "Branch"
+            options: "Branch",
         },
         {
+            fieldname: "department",
             label: __("Department"),
             fieldtype: "Link",
-            fieldname: "department",
-            options: "Department"
+            options: "Department",
         },
         {
+            fieldname: "employee",
             label: __("Employee"),
             fieldtype: "Link",
-            fieldname: "employee",
-            options: "Employee"
-        }
-    ],
+            options: "Employee",
+        },
 
-    onload: function(report) {
-        fetch_month_list();
-    }
+        // --- Mode: Single Month ---
+        {
+            fieldname: "month",
+            label: __("Month (Single)"),
+            fieldtype: "Select",
+            options: "",
+            description: "Select a single month. Overrides all other date filters.",
+            on_change: function () {
+                let report = frappe.query_report;
+                let val    = report.get_filter_value("month");
+                if (val) {
+                    report.set_filter_value("months", "");
+                    report.set_filter_value("from_date", "");
+                    report.set_filter_value("to_date", "");
+                }
+            }
+        },
+
+        // --- Mode: Multiple Months ---
+        {
+            fieldname: "months",
+            label: __("Months (Multi)"),
+            fieldtype: "Data",
+            placeholder: "e.g. Jan 2026, Feb 2026, Mar 2026",
+            description: "Comma separated months. Used when Single Month is not selected.",
+            on_change: function () {
+                let report = frappe.query_report;
+                let val    = report.get_filter_value("months");
+                if (val) {
+                    report.set_filter_value("month", "");
+                    report.set_filter_value("from_date", "");
+                    report.set_filter_value("to_date", "");
+                }
+            }
+        },
+
+        // --- Mode: Date Range ---
+        {
+            fieldname: "from_date",
+            label: __("From Date"),
+            fieldtype: "Date",
+            description: "Used when neither Single Month nor Multi Month is selected.",
+            on_change: function () {
+                let report = frappe.query_report;
+                let val    = report.get_filter_value("from_date");
+                if (val) {
+                    report.set_filter_value("month", "");
+                    report.set_filter_value("months", "");
+                }
+            }
+        },
+        {
+            fieldname: "to_date",
+            label: __("To Date"),
+            fieldtype: "Date",
+            on_change: function () {
+                let report = frappe.query_report;
+                let val    = report.get_filter_value("to_date");
+                if (val) {
+                    report.set_filter_value("month", "");
+                    report.set_filter_value("months", "");
+                }
+            }
+        },
+    ]
 };
-
-
-function fetch_month_list() {
-    frappe.call({
-        method: "gke_customization.gke_catalog.report.ot_report.ot_report.get_month_range",
-        freeze: false,
-        callback: function(r) {
-            var month = frappe.query_report.get_filter('month');
-            month.df.options.push(...r.message);
-            month.refresh();
-        }
-    });
-}
