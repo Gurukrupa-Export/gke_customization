d49d022e850969f4156342baae42146bd22cd0fb
sahilgiri_gk <girisahil2000@gmail.com>
2026-04-27 17:13:03 +0530

    Add 'Item Code' filter to Batch Wise Purchase Cycle

--- gke_customization/gke_catalog/report/batch_wise_purchase_cycle/batch_wise_purchase_cycle.js
+++ gke_customization/gke_catalog/report/batch_wise_purchase_cycle/batch_wise_purchase_cycle.js
@@ -1,68 +1,74 @@
 frappe.query_reports["Batch Wise Purchase Cycle"] = {
-	filters: [
-		{
-			fieldname: "material_request_id",
-			label: __("MR ID"),
-			fieldtype: "Link",
-			options: "Material Request",
-		},
-		{
-			fieldname: "po_id",
-			label: __("PO ID"),
-			fieldtype: "Link",
-			options: "Purchase Order",
-		},
-		{
-			fieldname: "pr_id",
-			label: __("PR ID"),
-			fieldtype: "Link",
-			options: "Purchase Receipt",
-		},
-		{
-			fieldname: "pi_id",
-			label: __("PI ID"),
-			fieldtype: "Link",
-			options: "Purchase Invoice",
-		},
-		{
-			fieldname: "batch_no",
-			label: __("Batch No"),
-			fieldtype: "Link",
-			options: "Batch",
-		},
-		{
-			fieldname: "company",
-			label: __("Company"),
-			fieldtype: "Link",
-			options: "Company",
-			default: frappe.defaults.get_user_default("Company"),
-		},
-		{
-			fieldname: "from_date",
-			label: __("From Date"),
-			fieldtype: "Date",
-			default: frappe.datetime.month_start(),
-		},
-		{
-			fieldname: "to_date",
-			label: __("To Date"),
-			fieldtype: "Date",
-			default: frappe.datetime.month_end(),
-		},
-	],
+    filters: [
+        {
+            fieldname: "material_request_id",
+            label: __("MR ID"),
+            fieldtype: "Link",
+            options: "Material Request",
+        },
+        {
+            fieldname: "po_id",
+            label: __("PO ID"),
+            fieldtype: "Link",
+            options: "Purchase Order",
+        },
+        {
+            fieldname: "pr_id",
+            label: __("PR ID"),
+            fieldtype: "Link",
+            options: "Purchase Receipt",
+        },
+        {
+            fieldname: "pi_id",
+            label: __("PI ID"),
+            fieldtype: "Link",
+            options: "Purchase Invoice",
+        },
+        {
+            fieldname: "item",
+            label: __("Item Code"),
+            fieldtype: "Link",
+            options: "Item",
+        },
+        {
+            fieldname: "batch_no",
+            label: __("Batch No"),
+            fieldtype: "Link",
+            options: "Batch",
+        },
+        {
+            fieldname: "company",
+            label: __("Company"),
+            fieldtype: "Link",
+            options: "Company",
+            default: frappe.defaults.get_user_default("Company"),
+        },
+        {
+            fieldname: "from_date",
+            label: __("From Date"),
+            fieldtype: "Date",
+            default: frappe.datetime.month_start(),
+        },
+        {
+            fieldname: "to_date",
+            label: __("To Date"),
+            fieldtype: "Date",
+            default: frappe.datetime.month_end(),
+        },
+    ],
 
-	formatter: function(value, row, column, data, default_formatter) {
-		value = default_formatter(value, row, column, data);
-		if (!data) return value;
+    formatter: function(value, row, column, data, default_formatter) {
+        value = default_formatter(value, row, column, data);
+        if (!data) return value;
 
-		if (column.fieldname === "pi_id" && data.pi_id) {
-			return `<span style="font-weight:600;">${value}</span>`;
-		}
+        if (column.fieldname === "pi_id" && data.pi_id) {
+            return `<span style="font-weight:600;">${value}</span>`;
+        }
 
-		if (column.fieldname === "pr_id" && data.pr_id) {
-			return `<span style="font-weight:600;">${value}</span>`;
-		}
+        if (column.fieldname === "pr_id" && data.pr_id) {
+            return `<span style="font-weight:600;">${value}</span>`;
+        }
 
-		return value;
-	}
+        return value;
+    }
 };
