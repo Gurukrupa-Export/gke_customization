278d75d0ffc409b852cece3969919ad21718b63e
Shubham Keshri <shubham_k@gkexport.com>
2026-07-29 12:51:28 +0530

    update Repair order form
    update Repair order form

--- gke_customization/gke_order_forms/doctype/repair_order_form/repair_order_form.js
+++ gke_customization/gke_order_forms/doctype/repair_order_form/repair_order_form.js
@@ -307,7 +307,7 @@ frappe.ui.form.on('Repair Order Form Detail', {
 		if (d.item & !d.is_jewelex_tag) {
 			console.log('hii');
 			frappe.call({
-				method: "gke_customization.gke_order_forms.doctype.order_form.order_form.get_bom_details",
+				method: "gke_customization.gke_order_forms.doctype.order_form.order_form.get_bom_detail",
 				args: {
 					"design_id": d.item,
 					"doc":d
