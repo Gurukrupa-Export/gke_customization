221256195849a41dcf01e9bbd81dd281074bd1ca
sandeep-m-gk <sandeep_m@gkexport.com>
2026-08-26 22:54:03 +0530

    dummy fixes for sadguru
    dummy fixes for sadguru

--- gke_customization/gke_catalog/report/accounts_payable___sd/accounts_payable___sd.py
+++ gke_customization/gke_catalog/report/accounts_payable___sd/accounts_payable___sd.py
@@ -5,7 +5,6 @@ from gke_customization.gke_catalog.report.accounts_receivable___sd.accounts_rece
 	ReceivablePayableReport,
 )
 
-
 def execute(filters=None):
 	args = {
 		"account_type": "Payable",
