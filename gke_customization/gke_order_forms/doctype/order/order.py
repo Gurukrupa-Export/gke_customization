c4869a93b76b22c542b54739af23a0120516e057
Kanishkar <kanishkar@aerele.in>
2026-09-16 14:02:00 +0530

    feat: custom flow type field to quotation mapping

--- gke_customization/gke_order_forms/doctype/order/order.py
+++ gke_customization/gke_order_forms/doctype/order/order.py
@@ -1817,6 +1817,7 @@ def make_quotation(source_name, target_doc=None):
 			"company": "company",
 			"party_name": "customer_code",
 			"order_type": "order_type",
+			"custom_flow_type": "flow_type",
 			"diamond_quality": "diamond_quality"
 		}
 		for target_field, source_field in field_map.items():
@@ -2077,6 +2078,7 @@ def make_quotation_fill_defaults(quotation, order):
 	quotation.company = order.company
 	quotation.party_name = order.customer_code
 	quotation.order_type = order.order_type
+	quotation.custom_flow_type = order.flow_type
 	quotation.diamond_quality = order.diamond_quality
 
 	service_types = frappe.db.get_values("Service Type 2", {"parent": order.name}, "service_type1")
